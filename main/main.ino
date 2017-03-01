876ee2b722e270fed730409390cff7c5a4677562
Conor Dunne <conor@cdunne.co.uk>
2017-03-01 17:52:14 -0500

    Rewrote logic.
    * Added support for noise reduction of alitude readings
    * Added basic velocity calculation
    * Added error margins
    * Added value that indicates apogee detection for parachute/airbrake deployment

--- main/main.ino
+++ main/main.ino
@@ -4,14 +4,33 @@
    Contributors:
 */
 
+#include <ResponsiveAnalogRead.h> // Import noise reduction library
+
 // Declare variables
-float currentAGL; //current above ground level altitude
-float lastAGL = 0; //last altitude reading (above ground level)
-float temperature; 
-float launchAlt = 0; //altitude at launch 
+int currentAGL; //current above ground level altitude
+int lastAGL = 0; //last altitude reading (above ground level)
+int maxAGL = -30000; // maximum altitude reading (above ground level)
+int calculatedMaxAGL;
+float temperature;
+float launchAlt = 0; //altitude at launch
 boolean isLaunched = false;
-unsigned long currentTime = millis(); //current time since Arduino started
-unsigned long timeAtLaunch = 0; //time since launch was detected
+boolean apogeeDetected = false;
+unsigned int currentTime; //current time since Arduino started
+unsigned int timeAtLaunch = 0; //time when launch was detected
+unsigned int timeAtApogee = 0; //time when apogee was detected
+float velocity = 0; // current vertical velocity with respect to the ground
+
+
+ResponsiveAnalogRead smoothAGL(0, false); // This will remove noise from the altitude
+
+
+// Constants
+const int LAUNCH_MARGIN = 2; // 2m needs to change in the positive direction before a launch to be detected
+const int APOGEE_MARGIN = 3; // 3m needs to change from the max alt for apogee detection.
+const int ERROR_MARGIN = 1; // 1m needs to change before it's detected
+
+const int MASS = 1; // mass of the rocket in kg
+const int AREA = 1; // cross-sectional area of the rocket in m^2
 
 
 void setup() {
@@ -28,7 +47,7 @@ void setup() {
   // Set launch altitude to ground altitude upon start
   launchAlt = getGroundAlt();
 
-  // Check Memory. If memory is less than 700, the log may fail. 
+  // Check Memory. If memory is less than 700, the log may fail.
   //The SD class requires a buffer of 500 bytes, plus another 200 for the stack
   //freeMem();
 
@@ -38,41 +57,58 @@ void setup() {
 }
 
 void loop() {
-  
-  // Set current Above Ground Level to current sensor reading
-  currentAGL = getAGLAlt();
-  
-  // get current time since Arduino launch
-  currentTime = millis();
-  
-    // If current AGL is 6m or above, then launch has been detected and record the time at launch	
-    if(currentAGL >= 6 && !isLaunched) { //signifies that the rocket has launched (6 meters)
+
+  // Only do the following if the time has increased
+  if (millis() > currentTime) {
+    // Get time since Arduino launch
+    currentTime = millis();
+
+    // Set current Above Ground Level to current sensor reading
+    lastAGL = currentAGL;
+    smoothAGL.update((int)getAGLAlt());
+    currentAGL = smoothAGL.getValue();
+
+    // Check for a launch: If the currentAGL is 2m greater than the launch margin, then the rocket has launched.
+    if (currentAGL >= LAUNCH_MARGIN && !isLaunched) {
       isLaunched = true;
-      timeAtLaunch = millis();
+      timeAtLaunch = currentTime;
     }
 
-    // If the rocket has launched
-    if(isLaunched) {
+    // If the rocket has launched, do the following:
+    if (isLaunched) {
 
-    	// If the rocket has reached apogee
-        if(currentAGL < lastAGL) {
-          
-          //TODO: logic incase we go faster than speed of sound
-          //print apogee for testing
-          //TODO: after testing fire signal to recovery
-          
-        }
-	
-	// Otherwise, continue and record current AGL to compare for the next iteration
-        else {
-          lastAGL = currentAGL;
-        }
+      // Get the current velocity (will become negative if apogee is detected)
+      if (apogeeDetected) {
+        velocity = -1 * getVelocity();
+      }
+      else {
+        velocity = getVelocity();
+      }
+
+      // Set max altitude to current altitude if current altitude is higher than the maximum alt reading
+      if (currentAGL >= (maxAGL + ERROR_MARGIN)) {
+        maxAGL = currentAGL;
+      }
+
+      // Check for apogee
+      if (currentAGL <= (maxAGL - APOGEE_MARGIN) && !apogeeDetected) {
+        apogeeDetected = true; // Parachute detection can use this
+        timeAtApogee = currentTime;
+        //TODO: logic incase we go faster than speed of sound
+      }
+
+      // Calculate suspected apogee
+      if (!apogeeDetected) {
+        // calculation goes here
+        calculatedMaxAGL = 10000;
+      }
+
+      
     }
-  
-  
+  }
   // write data to Log
-  writeLog(String(currentTime)+","+String(currentAGL)+","+String(lastAGL)+","+String(temperature)+","+
-    String(launchAlt)+","+String(isLaunched)+","+String(timeAtLaunch));
+  writeLog(String(currentTime) + F(",") + String(currentAGL) + F(",") + String(lastAGL) + F(",") + String(temperature) + F(",") +
+           String(launchAlt) + F(",") + String(isLaunched) + F(",") + String(timeAtLaunch) + F(",") + String(maxAGL) + F(",") + String(apogeeDetected));
 
 }
 
@@ -80,9 +116,9 @@ void loop() {
 float getGroundAlt() {
   float lowestAlt = 30000;
   float sampleAlt = 0;
-  for(int i = 0; i < 100; i++) {
+  for (int i = 0; i < 100; i++) {
     sampleAlt = get_alt();
-    if(sampleAlt < lowestAlt) 
+    if (sampleAlt < lowestAlt)
       lowestAlt = sampleAlt;
   }
   return lowestAlt;
@@ -92,6 +128,12 @@ float getGroundAlt() {
 float getAGLAlt() {
   return  get_alt() - launchAlt;
 }
+
+// get the current velocity
+float getVelocity() {
+  return (currentAGL / (currentTime - timeAtLaunch));
+}
+
 
 //uint16_t freeMem() {
 //  char top;
