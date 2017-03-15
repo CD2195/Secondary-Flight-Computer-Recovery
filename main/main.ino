ae38e35b713fba2c3d3d1674eed91eea987649b2
Conor Dunne <conor@cdunne.co.uk>
2017-03-15 18:14:12 -0400

    Added apogee calculation, parachute deployment signal and a time limit for apogee detection.

--- main/main.ino
+++ main/main.ino
@@ -15,6 +15,7 @@ float launchAlt = 0; //altitude at launch
 float temperature;
 boolean isLaunched = false;
 boolean apogeeDetected = false;
+boolean parachuteLaunched = false;
 unsigned long currentTime; //current time since Arduino started
 unsigned long timeAtLaunch = 0; //time when launch was detected
 unsigned long timeAtApogee = 0; //time when apogee was detected
@@ -27,16 +28,15 @@ float totalAccel; // total current acceleration
 
 
 ResponsiveAnalogRead smoothAGL(0, false); // This will remove noise from the altitude
-ResponsiveAnalogRead smoothAccel(0,false);
+ResponsiveAnalogRead smoothAccel(0, false); // Removes noise from acceleration
 
 
 // Constants
 const PROGMEM uint8_t LAUNCH_MARGIN = 2; // 2m needs to change in the positive direction before a launch to be detected
 const PROGMEM uint8_t APOGEE_MARGIN = 3; // 3m needs to change from the max alt for apogee detection.
 const PROGMEM uint8_t ERROR_MARGIN = 1; // 1m needs to change before it's detected
-
-const PROGMEM uint16_t MASS = 1; // mass of the rocket in kg
-const PROGMEM uint16_t AREA = 1; // cross-sectional area of the rocket in m^2
+const PROGMEM uint8_t APOGEE_TIME_LIMIT = 30; // in seconds
+const PROGMEM uint8_t SOUND_BARRIER = 343; // in m/s
 
 
 void setup() {
@@ -50,6 +50,10 @@ void setup() {
   initCard();
   initSensors();
 
+  // Initialise Parachute Signal Pin and set to low.
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+
   // Set launch altitude to ground altitude upon start
   launchAlt = getGroundAlt();
 
@@ -57,7 +61,7 @@ void setup() {
   updateAccel();
 
   // Create log header
-  writeLog(F("time,totalAccel,totalVel,verticalVel,currentAGL,lastAGL,temperature,launchAlt,isLaunched,apogeeDetected"));
+  writeLog(F("time,totalAccel,totalVel,verticalVel,currentAGL,lastAGL,temperature,launchAlt,isLaunched,calculatedApogee,apogeeDetected,parachuteLaunched"));
 
 }
 
@@ -75,12 +79,12 @@ void loop() {
 
     // Get the current acceleration
     updateAccel();
-    smoothAccel.update((int)(getTotalAccel()*100));
-    totalAccel = (smoothAccel.getValue())*0.01;
+    smoothAccel.update((int)(getTotalAccel() * 100));
+    totalAccel = (smoothAccel.getValue()) * 0.01;
 
     // Get velocities
-    totalVel = getVelocity(0,0);
-    verticalVel = getVelocity(0,3);
+    totalVel = getVelocity(0, 0);
+    verticalVel = getVelocity(0, 3);
 
     // Check for a launch: If the currentAGL is 2m greater than the launch margin, then the rocket has launched.
     if (currentAGL >= LAUNCH_MARGIN && !isLaunched) {
@@ -98,15 +102,32 @@ void loop() {
 
       // Check for apogee
       if (currentAGL <= (maxAGL - APOGEE_MARGIN) && !apogeeDetected) {
-        apogeeDetected = true; // Parachute detection can use this
+
+        if (getVelocity(0, 0) > SOUND_BARRIER) {
+          //TODO: logic incase we go faster than speed of sound
+        }
+        else {
+          apogeeDetected = true; // Parachute detection can use this
+          timeAtApogee = currentTime;
+        }
+      }
+      // If apogee is not detected beyond the time limit after launch, mark apogee event
+      else if (!apogeeDetected && ((currentTime - timeAtLaunch) >= (APOGEE_TIME_LIMIT * 1000))) {
+        apogeeDetected = true;
         timeAtApogee = currentTime;
-        //TODO: logic incase we go faster than speed of sound
+      }
+
+      // Send signal to pin 2 for backup release of parachute IF 5 seconds has elasped since apogee
+      if (apogeeDetected && (currentTime >= (timeAtApogee + 50000))) {
+        digitalWrite(2, HIGH);
+        parachuteLaunched = true;
       }
 
       // Calculate suspected apogee
       if (!apogeeDetected) {
         // calculation goes here
-        calculatedMaxAGL = 10000;
+        // alt = v^2 - v0^2 / 2a - if the rocket is coasting
+        calculatedMaxAGL = ((pow(getVelocity(0, 3), 2)) / ((2 * az))) + currentAGL;
       }
     }
     else {
@@ -114,9 +135,9 @@ void loop() {
     }
   }
   // write data to Log
-  writeLog(String(currentTime) + F(",") + String(totalAccel) + F(",") + String(totalVel) + F(",") + String(verticalVel) + 
-            F(",") + String(currentAGL) + F(",") + String(lastAGL) + F(",") + String(temperature) + F(",") + String(launchAlt) + 
-              F(",") + String(isLaunched) + F(",") + String(apogeeDetected));
+  writeLog(String(currentTime) + F(",") + String(totalAccel) + F(",") + String(totalVel) + F(",") + String(verticalVel) +
+           F(",") + String(currentAGL) + F(",") + String(lastAGL) + F(",") + String(temperature) + F(",") + String(launchAlt) +
+           F(",") + String(isLaunched) + F(",") + String(calculatedMaxAGL) + F(",") + String(apogeeDetected) + F(",") + String(parachuteLaunched));
 }
 
 // takes 100 samples and selects the lowest as ground altitude
@@ -124,7 +145,7 @@ float getGroundAlt() {
   float lowestAlt = 30000;
   float sampleAlt = 0;
   for (int i = 0; i < 100; i++) {
-    if(altitude_temperature_sensor(sampleAlt,temperature));
+    if (altitude_temperature_sensor(sampleAlt, temperature));
     else sampleAlt = 0;
     if (sampleAlt < lowestAlt)
       lowestAlt = sampleAlt;
@@ -135,7 +156,7 @@ float getGroundAlt() {
 // Get the altitude above ground level
 float getAGLAlt() {
   float altitude;
-  if(altitude_temperature_sensor(altitude,temperature)) {
+  if (altitude_temperature_sensor(altitude, temperature)) {
     return altitude - launchAlt;
   }
   return 0;
@@ -150,18 +171,18 @@ float getVelocity(float v0, byte direction) {
     case 2: accel = ay;
     case 3: accel = az;
   }
-  return (v0 + ((0.5)*(accel)*(pow(((currentTime- timeAtLaunch)*0.001),2))));
+  return (v0 + ((0.5) * (accel) * (pow(((currentTime - timeAtLaunch) * 0.001), 2))));
 }
 
 // Update acceleration from sensors
 void updateAccel() {
-  if(accelerometer_sensor(pitch,roll,heading,ax,ay,az));
+  if (accelerometer_sensor(pitch, roll, heading, ax, ay, az));
   else Serial.println("Accelerometer update failed!");
 }
 
 // get the current total acceleration
 float getTotalAccel() {
-  return sqrt((pow(ax,2)+pow(ay,2)+pow(az,2)));
+  return sqrt((pow(ax, 2) + pow(ay, 2) + pow(az, 2)));
 }
 
 //void get_accel() {
